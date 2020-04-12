cad7e5e6335b632ff0ad1c2d54be11d5f2b8bc15
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-12 22:37:22 +0900

    fix index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const express = require('express');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.BOT_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.BOT_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
+  channelSecret: process.env.LINE_BOT_CHANNEL_SECRET,
 };
 
 // create LINE SDK client
