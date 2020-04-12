bc39d5297ccf9f643d2fbf42fa85ce054b02372a
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-12 22:44:49 +0900

    fix index.js

--- index.js
+++ index.js
@@ -28,6 +28,20 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
