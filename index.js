a6ca8324b9d3eef8ded65457cd473f9041ab78ff
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-12 22:40:32 +0900

    fix index.js

--- index.js
+++ index.js
@@ -14,36 +14,12 @@ const client = new line.Client(config);
 
 // create Express app
 // about Express itself: https://expressjs.com/
-const app = express();
+server.listen(process.env.PORT || 3000);
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
-app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-}
-
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
+// -----------------------------------------------------------------------------
+// ルーター設定
+server.post('/callback', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
 });
