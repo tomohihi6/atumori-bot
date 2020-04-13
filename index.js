9b7d9bad6575ce5a3a6508e9749b47084685c0e6
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-13 15:36:17 +0900

    add comment

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 'use strict';
 
-const line = require('@line/bot-sdk');
+const line = require('@line/client-sdk');
 const express = require('express');
 const async = require('async');
 
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.LINE_BOT_CHANNEL_TOKEN,
-  channelSecret: process.env.LINE_BOT_CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_client_CHANNEL_TOKEN,
+  channelSecret: process.env.LINE_client_CHANNEL_SECRET,
 };
-
-// create LINE SDK client
-const client = new line.Client(config);
 
 // create Express app
 // about Express itself: https://expressjs.com/
 const app = express();
 
 // APIコールのためのクライアントインスタンスを作成
-const bot = new line.Client(config);
+const client = new line.Client(config);
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
@@ -28,9 +25,11 @@ app.post('/callback', line.middleware(config), (req, res) => {
     // すべてのイベント処理のプロミスを格納する配列。
     let events_processed = [];
 
+    //パターンにないメッセージが来た時にランダムに返信メッセージを決める
     async function tempResponse(e) {
+        //おそらくプロフィール情報の取得に時間がかかってnameにundefindが入ることがあるので待つ
         let name = await getUserName(e.source.userId);
-        console.log(`名前は${name}`)
+        // console.log(`名前は${name}`)
         const tempTexts = [
             "会話実装めんどくさすぎてはげそうだなも!",
             "ぼくと話す前に早く借金返せだなも！",
@@ -39,33 +38,42 @@ app.post('/callback', line.middleware(config), (req, res) => {
             `ぼくに騙されて${name}さんが無人島ツアーに申し込んでくれたおかげで，人生勝ち組だなも`
         ]
         let random = Math.floor( Math.random() * tempTexts.length );
-        events_processed.push(bot.replyMessage(e.replyToken, {
+        events_processed.push(client.replyMessage(e.replyToken, {
             type: "text",
             text: tempTexts[random]
         }));  
     }
+
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+            //数字だけのテキストかどうかを判定
             let numFlug = true;
             for(let i = 0; i < event.message.text.length; i++) {
+                //1文字ずつアスキーコードを比較して数字判定
                 let charCode = event.message.text.charCodeAt(i);
                 if(charCode < 48  || charCode > 57){
                     numFlug = false;
+                    //１つでも数字以外の文字が見つかった場合for文終わり
                     break;
                 }
             }
             if(numFlug) {
-                events_processed.push(bot.replyMessage(event.replyToken, {
+                let date = new Date();
+                let hour = date.getHours();
+                let ampm = (hour < 12) ? "午前" : "午後";
+                
+                events_processed.push(client.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "株価の記録だなもね？"
+                    text: "株価を記録しただなも"
                 }))
+
+                //数字以外のテキストの処理    
             } else {
                 switch (event.message.text) {
                     case "こんにちは":
-                        events_processed.push(bot.replyMessage(event.replyToken, {
+                        events_processed.push(client.replyMessage(event.replyToken, {
                             type: "text",
                             text: "どうもだなも!"
                         }));
@@ -80,14 +88,14 @@ app.post('/callback', line.middleware(config), (req, res) => {
                         var dayOfWeek = date.getDay();
                         var dayOfWeekStr = [ "日", "月", "火", "水", "木", "金", "土" ][dayOfWeek] ;
                         const time = `今は${month}月${day}日の${dayOfWeekStr}曜日${hour}時${minute}分だなも`
-                        events_processed.push(bot.replyMessage(event.replyToken, {
+                        events_processed.push(client.replyMessage(event.replyToken, {
                             type: "text",
                             text: time
                         }))
                         break;
     
                     case "しずえは？":
-                        events_processed.push(bot.replyMessage(event.replyToken, {
+                        events_processed.push(client.replyMessage(event.replyToken, {
                             type: "text",
                             text: "ノーコメントだなも"
                         }));
@@ -130,6 +138,10 @@ async function getUserName(userID) {
     const userId = userID;
     const pro = await client.getProfile(userId)
     return pro.displayName;
+}
+
+function getCurrentTime() {
+    const 
 }
 
 // listen on port
