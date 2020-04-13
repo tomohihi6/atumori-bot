5a7bf50191c599644fa2f85e05bb01b6e58d7fb4
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-13 23:51:16 +0900

    Improve

--- database.js
+++ database.js
@@ -2,9 +2,9 @@ const {Pool} = require('pg');
 require('dotenv').config();
 
 exports.pool = new Pool({
-    user: process.env.ENV_USER,
+    user: process.env.ENV_DB,
     host: process.env.ENV_HOST,
-    databese: process.env.ENV_USER,
+    databese: process.env.ENV_DB,
     password: process.env.ENV_PASSWORD,
     port: 5432,
 })
