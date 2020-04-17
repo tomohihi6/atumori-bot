e0074cc0deb610aa8d9b320c1c28630c3fe638d5
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-04-18 01:59:37 +0900

    Improve

--- index.js
+++ index.js
@@ -313,14 +313,18 @@ app.post('/callback', line.middleware(config), (req, res) => {
                             const query = `SELECT leftover FROM leftover_tb;`;
                             fetchFromDatabase(query)
                             .then((res) => {
-                                let replyText = "";
-                                for(let i = 0; i < res.rows.length; i++) {
-                                    replyText += `${res.rows[i].leftover}`
-                                    if(i !== res.rows.length - 1)　{
-                                        replyText += `\n`;
-                                    }                                    
+                                if(res.rowCount != 0) {
+                                    let replyText = "";
+                                    for(let i = 0; i < res.rows.length; i++) {
+                                        replyText += `${res.rows[i].leftover}`
+                                        if(i !== res.rows.length - 1)　{
+                                            replyText += `\n`;
+                                        }                                    
+                                    }
+                                    replyMessage(event, replyText);
+                                } else {
+                                    replyMessage(event, "そんなもんねえよ");
                                 }
-                                replyMessage(event, replyText);
                             }).catch((err) => {
                                 console.log(err);
                                 replyMessage(event, "余り物リスト取得に失敗しただなも");
