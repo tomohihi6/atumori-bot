2841d3d634c85f9f3fbcc3d28a2e3f2be3273ef4
Tomohiro Kawatani <50481053+tomohihi6@users.noreply.github.com>
2020-04-30 15:52:39 +0900

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-const {Pool} = require('pg');
-
-exports.pool = new Pool({
-    user: process.env.ENV_USER,
-    host: process.env.ENV_HOST,
-    databese: process.env.ENV_DB,
-    password: process.env.ENV_PASSWORD,
-    port: 5432,
-})
